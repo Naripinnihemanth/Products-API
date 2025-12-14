bdd2437fd03a4e80a4aee34472503f92ec59bed0
hemanth naripinni <hemanthnaripinni125@gmail.com>
2025-12-14 12:14:20 +0530

    build.py changed

--- build.sh
+++ build.sh
@@ -6,7 +6,16 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[$CREATE_SUPERUSER]];
-then
-    python manage.py createsuperuser --no-input
-fi
+python manage.py shell << EOF
+from django.contrib.auth.models import User
+import os
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+
+if username and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=os.environ.get("DJANGO_SUPERUSER_EMAIL"),
+        password=os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+    )
+EOF
